2f7726739d379bca74107eff2ccb8e297c8920cf
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2016-10-17 21:01:19 +0200

    increase test coverage

--- test/sendFile.js
+++ test/sendFile.js
@@ -3,6 +3,7 @@
 // todo: test errors and multiple signatures
 
 const fs      = require('fs'),
+      http    = require('http'),
       request = require('supertest'),
       test    = require('tap'),
       text    = fs.readFileSync(__filename, 'utf8'),
@@ -31,16 +32,25 @@ app.get('/3', function *(next) {
     test.same(calls, [ 1, 2 ], 'response should be sent first (promise)')
 })
 
+app.get('/headers', (req, res) => {
+    res.sendFile(__filename, {
+        headers: {
+            'x-test-1': 1,
+            'x-test-2': 'test'
+        }
+    })
+})
+
 const error = app.mount('/error')
 
-error.get('/1', (req, res) => {
+error.get('/basic', (req, res) => {
     res.sendFile('/non/existing').catch(err => {
         test.type(err, Error, 'an error should be passed back (catch)')
         test.equals(err.code, 'ENOENT', 'error should be ENOENT (catch)')
     })
 })
 
-error.get('/2', (req, res) => {
+error.get('/callback', (req, res) => {
     res.sendFile('/non/existing', err => {
         test.type(err, Error, 'an error should be passed back (callback)')
         test.equals(err.code, 'ENOENT', 'error should be ENOENT (callback)')
@@ -48,7 +58,7 @@ error.get('/2', (req, res) => {
     })
 })
 
-error.get('/3', function *(next) {
+error.get('/yield', function *(next) {
     try {
         yield this.sendFile('/non/existing')
     }
@@ -60,26 +70,53 @@ error.get('/3', function *(next) {
     }
 })
 
-test.plan(14)
+error.get('/directory', (req, res) => {
+    res.sendFile(__dirname, err => {
+        test.type(err, Error, 'an error should be passed back (directory)')
+        test.equals(err.code, 'EISDIR', 'error should be EISDIR (directory)')
+        res.status(404)
+        res.send('error')
+    })
+})
 
+error.get('/abort', (req, res) => {
+    res.sendFile(__filename, err => {
+        test.type(err, Error, 'an error should be passed back (abort)')
+        test.equals(err.code, 'ECONNABORTED', 'error should be ENOENT (abort)')
+    })
+    res.end()
+})
+
+test.plan(21)
 app = app.listen()
-
 test.tearDown(() => app.close())
 
 get('/1')
 get('/2')
 get('/3')
-getError('/error/1')
-getError('/error/2')
-getError('/error/3')
+get('/headers', [ 'x-test-1', 'x-test-2' ])
+getError('/error/basic')
+getError('/error/callback')
+getError('/error/yield')
+getError('/error/directory')
+getErrorAbort('/error/abort')
 
-function get(path) {
-    request(app)
+function get(path, headers) {
+    const req = request(app)
         .get(path)
         .expect(200)
         .expect('content-type', 'application/javascript')
         .expect('content-length', length)
-        .expect(text, err => {
+
+    if (headers)
+        headers.forEach(h => {
+            req.expect(res => {
+                if (!(h in res.headers))
+                    throw new Error(h + ' header expected, but missing')
+            })
+        })
+
+    req.expect(text, err => {
             if (err)
                 test.threw(err)
             else
@@ -95,5 +132,18 @@ function getError(path) {
                 test.threw(err)
             else
                 test.pass('error response received')
+        })
+}
+
+function getErrorAbort(path) {
+    request(app)
+        .get(path)
+        // 200, beacause the server doesn't have a chance to
+        // change headers which are already sent
+        .expect(200, err => {
+            if (err)
+                test.threw(err)
+            else
+                test.pass('abort response received')
         })
 }
