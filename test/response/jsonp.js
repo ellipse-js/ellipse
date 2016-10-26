c26a5cca4bf0b78189ca01bc49bdd7a6f7ef58a5
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2016-10-26 18:29:16 +0200

    add `res.jsonp()`
    Warn users coming from Express about missing jsonp() method.

---
+++ test/response/jsonp.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const test    = require('tap'),
+      utils   = require('../utils'),
+      end     = utils.end,
+      create  = utils.create,
+      request = utils.request
+
+test.test('.jsonp() should throw', test => {
+    const app = create().use((req, res) => {
+        test.throws(() => {
+            res.jsonp()
+        })
+
+        res.send('ok')
+    })
+
+    request(app)
+        .get('/')
+        .expect(200, 'ok', end(test))
+})
