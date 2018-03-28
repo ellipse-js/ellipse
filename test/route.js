43d830e83b0bfe387f002afaac297f80618f2abe
Schwarzkopf Balázs <schwarzkopfb@icloud.com>
2018-03-28 16:22:31 +0200

    optimise OPTIONS handling

--- test/route.js
+++ test/route.js
@@ -11,7 +11,7 @@ test.equals(route.router, app, 'route.router should be set')
 test.equals(route.length, 1, 'route.length should represent the handler count of a route')
 test.equals(route.method, 'GET', 'route.method should be set')
 test.equals(route.path, '/', 'route.path should be set')
-test.equals(route.all, false, 'route.all should be set')
+test.equals(route.isAll, false, 'route.isAll should be set')
 test.equals(route.wildcard, false, 'route.wildcard should be set')
 
 const expected = {
